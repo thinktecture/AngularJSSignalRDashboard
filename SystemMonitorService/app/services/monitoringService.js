bb4241a383604821a622f3c066c6a95889ef250b
Christian Weyer <christian.weyer@thinktecture.com>
2014-05-13 22:22:02 +0200

    Added re-connecting

--- SystemMonitorService/app/services/monitoringService.js
+++ SystemMonitorService/app/services/monitoringService.js
@@ -1,17 +1,24 @@
-﻿myApp.factory("monitoringService", function ($http) {
+﻿myApp.factory("monitoringService", function ($timeout) {
     var service = {};
 
     var connection = $.hubConnection();
     connection.logging = true;
 
     var hubProxy = connection.createHubProxy("performanceDataHub");
-    
+
     service.init = function (scope) {
         hubProxy.on("newCpuValue", function (data) {
             scope.$broadcast("newCpuValue", data);
         });
         hubProxy.on("newDiskValue", function (data) {
             scope.$broadcast("newDiskValue", data);
+        });
+
+        connection.disconnected(function () {
+            $timeout(function () {
+                connection.start().done(function () {
+                });
+            }, 5000);
         });
 
         connection.start().done(function () {
